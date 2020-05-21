261a6f5fda0e0d4b95c143ebf534060368dfd3e6
monadplus <arnauabella@gmail.com>
2020-05-21 17:30:47 +0200

    WIP hw2: first and second exercise finished

--- hw2/ex1/Main.hs
+++ hw2/ex1/Main.hs
@@ -1,3 +1,5 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i runghc
 {-# LANGUAGE DeriveAnyClass     #-}
 {-# LANGUAGE DeriveGeneric      #-}
 {-# LANGUAGE DerivingStrategies #-}
@@ -12,33 +14,33 @@ import           System.Random.MWC
 import           System.Random.MWC.Distributions
 
 data Individual = Individual
-  { unFactor1  :: Double
-  , unFactor2  :: Double
-  , unFactor3  :: Double
-  , unFactor4  :: Double
-  , unFactor5  :: Double
-  , unFactor6  :: Double
-  , unFactor7  :: Double
-  , unFactor8  :: Double
-  , unFactor9  :: Double
-  , unFactor10 :: Double
-  , unAnswer   :: Double
+  { factor1  :: Double
+  , factor2  :: Double
+  , factor3  :: Double
+  , factor4  :: Double
+  , factor5  :: Double
+  , factor6  :: Double
+  , factor7  :: Double
+  , factor8  :: Double
+  , factor9  :: Double
+  , factor10 :: Double
+  , answer   :: Double
   } deriving stock (Show, Generic)
-    deriving anyclass (ToRecord)
+    deriving anyclass (ToRecord, ToNamedRecord, DefaultOrdered)
 
 generateIndividual :: IO Individual
 generateIndividual = withSystemRandom . asGenIO $ \gen -> do
-  f1 <- standard gen
-  f2 <- uniform gen
-  f3 <- exponential 0.5 gen
-  f4 <- standard gen
-  f5 <- uniform gen
+  f1 <- abs <$> standard gen
+  f2 <- abs <$> uniform gen
+  f3 <- abs <$> exponential 0.5 gen
+  f4 <- abs <$> standard gen
+  f5 <- abs <$> uniform gen
   let f6 = f1 + f2
-      f7 = f1 + f3*2
-      f8 = f3 - f2
-      f9 = f1 + f2 + f3
-      f10 = f1 + f2 - f3
-  answer <- (*) (f1 + 2*f2 - f6 + f7 + 3*f8 + f9 + f10) <$> standard gen
+      f7 = f1 + 2*f3
+      f8 = f1 + f4
+      f9 = f4 + 5*f5
+      f10 = f1 + f2 + f3 + f4 + f5
+  answer <- (f1 + f2 + f9 +) <$> standard gen
   return (Individual f1 f2 f3 f4 f5 f6 f7 f8 f9 f10 answer)
 
 generateDataset :: Int -> IO [Individual]
@@ -49,4 +51,4 @@ generateDataset n = do
   return (x : xs)
 
 main :: IO ()
-main = (writeFile "dataset.csv" . encode) =<< generateDataset 2000
+main = (writeFile "dataset.csv" . encodeDefaultOrderedByName) =<< generateDataset 2000
